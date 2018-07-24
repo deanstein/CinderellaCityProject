aed6eb663d8ddfb9d31abc7f98650422934665f6
Josh Goldstein <jdeangoldstein@gmail.com>
2018-07-24 17:54:28 -0600

    Adding initial game files

---
+++ 20180610 unity interaction test/Assets/TogglePresence.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePresence : MonoBehaviour {
+
+    public GameObject targetObjects;
+    public GameObject UIElement;
+
+    // Use this for initialization
+    void Start () {
+
+        targetObjects = GameObject.FindWithTag("Mall1980s");
+        UIElement = GameObject.FindWithTag("UI");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // if key is pressed, turn on or off a target elementf
+        if ((Input.GetKeyDown("h")) && (targetObjects.activeSelf)) {
+            targetObjects.SetActive(false);
+            Debug.Log("Turning OFF objects: " + targetObjects);
+        }
+
+        else if ((Input.GetKeyDown("h")) && (!targetObjects.activeSelf)) {
+            targetObjects.SetActive(true);
+            Debug.Log("Turning ON objects: " + targetObjects);
+        }
+
+        }
+
+    /*void OnGUI()
+    {
+        GUI.
+        if (GUI.Button(UIElement, "I am a button"))
+        {
+            Debug.Log("You clicked the button!");
+        }
+    }*/
+
+}
+
---
+++ Cinderella City Project Master/Assets/RecalibrateOrigin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetOriginToBoundingBoxCenter : MonoBehaviour {
+
+    public GameObject speakerObjects;
+
+    // Use this for initialization
+    void Start () {
+
+        // get objects with the correct tag
+        speakerObjects = GameObject.FindWithTag("speakers");
+        Debug.Log("Found these objects tagged as speaker objects: " + speakerObjects);
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log("Turning OFF objects: " + speakerObjects);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
