789089b19e095bde3642145e4175b97cb096ef49
Josh <jdeangoldstein@gmail.com>
2019-08-17 16:03:20 -0600

    Restoring speaker audio, adding new signage, stores, and lights
    Logic Updates:
    - Added the ability to attach Unity behavior components like AudioSource to GameObjects
    - Added the ability to attach custom scripted behavior components like PlayAudioSequencesByName to GameObjects
    - Added PlayAudioSequencesByName script, created new audio sequences from music clips and applied them to speaker models
    - Renamed TogglePeople to ToggleVisibilityWithShortcut, and it's now automatically applied to proxy-people.fbx
    
    Asset Updates:
    - Added 701 Offices, Americana Shop, American Waterbed, Bermans, B. Dalton, Brooks, Co. 6, Cristy, DEB, Dairy Queen, Chick-Fil-A, Corn Dog, Jarman, J. Brannam, Lee's Hallmark, Lerner Shops, Mastercuts, Naturalizer Shoes, nina B, Payless Shoes, Paul Wu's, Sbarro, Sportsfan, Stuarts, Tandy Leather, Thom McAn, Ups N Downs, Waldenbooks, Zales, Zeezos signage to the model
    - Updated Cinder Alley landmark wayfinding with better vector graphics, added Cinder Alley east wall wayfinding
    - Updated Foot Locker logo with more accurate linework
    - Added Joslins signage variations to Cinder Alley
    - Added Broadway Southwest signage variation to Shamrock Mall
    - Added mall colored wayfinding signage
    - Added 3-sided mall directories
    - Added more lights to Cinder Alley near JC Penney
    - Added site lighting, including above- and below-grade parking fixtures
    - Added vacant storefronts to Shamrock Mall and Cinder Alley
    - Added starter storefronts to Cinder Alley
    - Fixed and detailed east Cinder Alley stair near Joslins
    - Added perimeter string lights and corner ceiling lights in food court
    - Switched Broadway signage model back to side-lit
    - Watertight fixes in some models
    - Added new music by Betamaxx, more tracks by DeadMall, and organized all audio files per-era
    - Updated speaker model naming and organization for new AudioSource code
    - Added speakers in Rose and Gold Malls, reorganized and moved existing speakers in Blue Mall
    - Restored old speaker types
    - Added support for new speaker types, including ambient sound,
    - Fixed issues with handrails, including filling in corners at Blue Mall
    - Known issue: context site model will not render (Unity crashes)

---
+++ CinderellaCitySimulation/Assets/Scripts/PlayAudioSequencesByName.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class PlayAudioSequencesByName : MonoBehaviour {
+
+    // Use this for initialization
+    void Start ()
+    {
+        // get the audio source for this GameObject
+        AudioSource audioSourceComponent = this.GetComponent<AudioSource>();
+        //Debug.Log("This audio source: " + audioSourceComponent);
+
+        // set default audio source options
+        audioSourceComponent.spatialBlend = 1.0F;
+        audioSourceComponent.dopplerLevel = 0F;
+        audioSourceComponent.rolloffMode = AudioRolloffMode.Custom;
+        audioSourceComponent.loop = true;
+
+        // set volumes based on type
+        float mallAmbientChatterSpeakerVolume = 0.15F;
+        float mallMusicSpeakerVolume = 0.25F;
+        float storeMusicSpeakerVolume = 0.25F;
+
+        // set max distances based on type
+        float mallAmbientChatterSpeakerMaxDistance = 500F;
+        float mallMusicSpeakerMaxDistance = 20F;
+        float storeMusicSpeakerMaxDistance = 15F;
+
+
+        //
+        // define the clips that should be played per area
+        //
+
+        // mall
+        string[] mallAmbientChatter80s90sAudioClips = { "80s-90s/Ambient/BDalton/CinCityAmbience" };
+        string[] mallMusic80s90sAudioClips = { "80s-90s/Music/Betamaxx/6. woolworth", "80s-90s/Music/Betamaxx/1. grand opening", "80s-90s/Music/Betamaxx/8. mall walking", "80s-90s/Music/BDalton/nick", "80s-90s/Music/Betamaxx/7. crystal fountain", "80s-90s/Music/BDalton/uhh" };
+
+        // stores
+        string[] storeMusicGeneric80s90sAudioClips = { "80s-90s/Music/Betamaxx/9. a'gaci", "80s-90s/Music/Betamaxx/10. lazarus", "80s-90s/Music/Betamaxx/5. kauffmans" };
+        string[] storeMusicConsumerBeauty80s90sAudioClips = { "80s-90s/Music/DeadMall/05 Thunderhead" };
+        string[] storeMusicDolcis80s90sAudioClips = { "80s-90s/Music/Betamaxx/5. kauffmans", "80s-90s/Music/Betamaxx/10. lazarus" };
+        string[] storeMusicMusicland80s90sAudioClips = { "80s-90s/Music/DeadMall/01 Wheels", "80s-90s/Music/DeadMall/09 Pressure" };
+
+
+        //
+        // play the clip sequences based on the object name
+        //
+
+        if (this.name.Contains("mall-ambient-chatter-80s90s"))
+        {
+            audioSourceComponent.volume = mallAmbientChatterSpeakerVolume;
+            audioSourceComponent.maxDistance = mallAmbientChatterSpeakerMaxDistance;
+            StartCoroutine(playClipSequenceInOrder(audioSourceComponent, mallAmbientChatter80s90sAudioClips));
+        }
+
+        if (this.name.Contains("mall-music-80s90s"))
+        {
+            audioSourceComponent.volume = mallMusicSpeakerVolume;
+            audioSourceComponent.maxDistance = mallMusicSpeakerMaxDistance;
+            StartCoroutine(playClipSequenceInOrder(audioSourceComponent, mallMusic80s90sAudioClips));
+        }
+
+        if (this.name.Contains("store-music-generic-80s90s"))
+        {
+            audioSourceComponent.volume = storeMusicSpeakerVolume;
+            audioSourceComponent.maxDistance = storeMusicSpeakerMaxDistance;
+            StartCoroutine(playClipSequenceInOrder(audioSourceComponent, storeMusicGeneric80s90sAudioClips));
+        }
+
+        if (this.name.Contains("store-music-dolcis-80s90s"))
+        {
+            audioSourceComponent.volume = storeMusicSpeakerVolume;
+            audioSourceComponent.maxDistance = storeMusicSpeakerMaxDistance;
+            StartCoroutine(playClipSequenceInOrder(audioSourceComponent, storeMusicDolcis80s90sAudioClips));
+        }
+
+        if (this.name.Contains("store-music-musicland-80s90s"))
+        {
+            audioSourceComponent.volume = storeMusicSpeakerVolume;
+            audioSourceComponent.maxDistance = storeMusicSpeakerMaxDistance;
+            StartCoroutine(playClipSequenceInOrder(audioSourceComponent, storeMusicMusicland80s90sAudioClips));
+        }
+
+    }
+
+    IEnumerator playClipSequenceInOrder(AudioSource audioSourceComponent, string[] clipNames)
+    {
+        // for each clip in the array, play it, and wait until the end to play the next
+        for (var i = 0; i < clipNames.Length; i++)
+        {
+            AudioClip clip = (AudioClip)Resources.Load(clipNames[i]);
+            audioSourceComponent.clip = clip;
+            audioSourceComponent.Play();
+            Debug.Log("Playing music: " + clip.name);
+
+            yield return new WaitForSeconds(clip.length);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
