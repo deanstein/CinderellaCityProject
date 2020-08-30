38230f652ea32f48c5a1bc8e57dda12931b79f96
Josh <jdeangoldstein@gmail.com>
2020-08-30 17:18:06 -0600

    Fixing build error caused by Editor function in Player context
    - Created a new ManageEditorScenes file to store Editor-only scene operations, since these cannot live in the Player context

---
+++ CinderellaCitySimulation/Assets/Scripts/Editor/ManageEditorScenes.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+
+public static class ManageEditorScenes
+{
+    // load the given scenes additively in the Unity Editor, starting from a given loading scene
+    // closes the current scene if the current scene is not the specified loading scene
+    public static void LoadEditorScenesAdditively(string loadingScene, string[] sceneNames)
+    {
+        // ensure the loadingScene is opened first
+        if (SceneManager.GetActiveScene().name != loadingScene)
+        {
+            string loadingScenePath = SceneGlobals.GetScenePathByName(loadingScene);
+
+            EditorSceneManager.OpenScene(loadingScenePath);
+            Utils.DebugUtils.DebugLog("Opening the loading scene first...");
+        }
+
+        // load each specified scene additively
+        for (var i = 0; i < sceneNames.Length; i++)
+        {
+            // convert the scene name to a path
+            string scenePath = SceneGlobals.GetScenePathByName(sceneNames[i]);
+
+            EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Additive);
+            Utils.DebugUtils.DebugLog("Additively opening scene in editor: " + scenePath);
+        }
+    }
+
+}
+
