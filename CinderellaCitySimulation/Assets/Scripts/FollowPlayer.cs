b29c13b01d09091e6d6fefb15860c9873d5b23e1
Josh <jdeangoldstein@gmail.com>
2024-04-22 12:10:13 -0600

    Fist pass of fixing issue with player transform in FollowPlayer

--- CinderellaCitySimulation/Assets/Scripts/FollowPlayer.cs
+++ CinderellaCitySimulation/Assets/Scripts/FollowPlayer.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 // Responsible for making an object's position match the current player's position at all times
 //attach this script to an object that needs to follow the player
@@ -7,6 +8,7 @@ public class FollowPlayer : MonoBehaviour
 {
     private void Update()
     {
+        // only follow player if guided tour is NOT active
         if (!ManageFPSControllers.FPSControllerGlobals.isGuidedTourActive)
         {
             if (ManageFPSControllers.FPSControllerGlobals.activeFPSControllerTransform != null)
@@ -14,11 +16,19 @@ public class FollowPlayer : MonoBehaviour
                 // update this object's position to match the player's last known position
                 this.transform.position = ManageFPSControllers.FPSControllerGlobals.activeFPSControllerTransform.position;
             }
-        }
-        // make the player follow the agent's location
-        else
+            // if guided tour is active, FPSController must follow agent
+        } else
         {
-            ManageFPSControllers.FPSControllerGlobals.activeFPSControllerTransform.position = this.transform.position;
+            // this is required to be false for NavMeshAgent to not interfere
+            this.GetComponent<NavMeshAgent>().updatePosition = false;
+
+            // TODO: decide which of the below we want to use
+
+            // move the FPSController transform to the NavMeshAgent's next position
+            //ManageFPSControllers.FPSControllerGlobals.activeFPSControllerTransform.position = this.GetComponent<NavMeshAgent>().nextPosition;
+
+            // use simplemove to chase the NavMeshAgent
+            this.transform.parent.GetComponentInChildren<CharacterController>().SimpleMove(this.GetComponent<NavMeshAgent>().velocity);
         }
     }
 }
