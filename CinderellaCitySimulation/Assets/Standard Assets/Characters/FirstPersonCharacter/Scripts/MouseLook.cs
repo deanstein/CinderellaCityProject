3a923c42531b84389e9668d12d2356cb9ff50b18
Josh <jdeangoldstein@gmail.com>
2024-05-17 12:29:59 -0600

    Updating standard MouseLook asset to not interfere with controller

--- CinderellaCitySimulation/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ CinderellaCitySimulation/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -30,29 +30,32 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         public void LookRotation(Transform character, Transform camera)
         {
-            float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
-            float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
-
-            m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
-            m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
-
-            if(clampVerticalRotation)
-                m_CameraTargetRot = ClampRotationAroundXAxis (m_CameraTargetRot);
-
-            if(smooth)
+            if (CrossPlatformInputManager.GetAxis("Mouse X") != 0 || CrossPlatformInputManager.GetAxis("Mouse Y") != 0)
             {
-                character.localRotation = Quaternion.Slerp (character.localRotation, m_CharacterTargetRot,
-                    smoothTime * Time.deltaTime);
-                camera.localRotation = Quaternion.Slerp (camera.localRotation, m_CameraTargetRot,
-                    smoothTime * Time.deltaTime);
-            }
-            else
-            {
-                character.localRotation = m_CharacterTargetRot;
-                camera.localRotation = m_CameraTargetRot;
-            }
+                float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
+                float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
 
-            UpdateCursorLock();
+                m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
+                m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
+
+                if(clampVerticalRotation)
+                    m_CameraTargetRot = ClampRotationAroundXAxis (m_CameraTargetRot);
+
+                if(smooth)
+                {
+                    character.localRotation = Quaternion.Slerp (character.localRotation, m_CharacterTargetRot,
+                        smoothTime * Time.deltaTime);
+                    camera.localRotation = Quaternion.Slerp (camera.localRotation, m_CameraTargetRot,
+                        smoothTime * Time.deltaTime);
+                }
+                else
+                {
+                    character.localRotation = m_CharacterTargetRot;
+                    camera.localRotation = m_CameraTargetRot;
+                }
+
+                UpdateCursorLock();
+            }
         }
 
         public void SetCursorLock(bool value)
