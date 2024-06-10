503190c636fd4bb7d48c97855220a6643e374607
Josh <jdeangoldstein@gmail.com>
2024-06-09 22:19:59 -0600

    Fixing issue where agent would still disconnect from player

--- CinderellaCitySimulation/Assets/Scripts/FollowPlayerOrAgent.cs
+++ CinderellaCitySimulation/Assets/Scripts/FollowPlayerOrAgent.cs
@@ -15,7 +15,22 @@ public class FollowPlayerOrAgent : MonoBehaviour
             if (ManageFPSControllers.FPSControllerGlobals.activeFPSControllerTransform != null)
             {
                 // update this object's position to match the player's last known position
-                GetComponent<NavMeshAgent>().nextPosition = ManageFPSControllers.FPSControllerGlobals.activeFPSControllerTransform.position;
+                GetComponent<NavMeshAgent>().transform.position = ManageFPSControllers.FPSControllerGlobals.activeFPSControllerTransform.position;
+
+                // it's possible for the agent to still get separated from the player
+                // for example, if the player jumped over a rail
+                // if agent gets too far from player, disable the agent to unlock it from the navmesh
+                bool isOnNavMesh = Utils.GeometryUtils.GetIsOnNavMeshWithinTolerance(new Vector3(ManageFPSControllers.FPSControllerGlobals.activeFPSControllerTransform.position.x, ManageFPSControllers.FPSControllerGlobals.activeFPSControllerTransform.position.y - ManageFPSControllers.FPSControllerGlobals.activeFPSControllerNavMeshAgent.baseOffset / 2, ManageFPSControllers.FPSControllerGlobals.activeFPSControllerTransform.position.z), 1.0f);
+
+                if (!ManageFPSControllers.FPSControllerGlobals.activeFPSController.GetComponentInChildren<CharacterController>().isGrounded || !isOnNavMesh)
+                {
+                    GetComponent<NavMeshAgent>().enabled = false;
+                // otherwise, ensure the agent is enabled
+                } else
+                {
+                    GetComponent<NavMeshAgent>().enabled = true;
+                }
+
                 this.GetComponent<NavMeshAgent>().updatePosition = true;
             }
             // if guided tour is active, FPSController must follow agent
@@ -23,6 +38,9 @@ public class FollowPlayerOrAgent : MonoBehaviour
         {
             if (ManageFPSControllers.FPSControllerGlobals.activeFPSControllerTransform != null)
             {
+                // always make sure the agent is on
+                this.GetComponent<NavMeshAgent>().enabled = true;
+
                 // this is required to be false for NavMeshAgent to not interfere
                 this.GetComponent<NavMeshAgent>().updatePosition = false;
 
