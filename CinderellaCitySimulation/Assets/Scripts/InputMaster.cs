36312de213bface235c8bfd7459b53e195425a9a
Josh <jdeangoldstein@gmail.com>
2024-06-23 22:18:11 -0600

    Adding additional actions to controller

--- CinderellaCitySimulation/Assets/Scripts/InputMaster.cs
+++ CinderellaCitySimulation/Assets/Scripts/InputMaster.cs
@@ -19,16 +19,8 @@ public class @InputMaster : IInputActionCollection, IDisposable
             ""id"": ""b0cabd14-0965-4b8b-b30f-bb53eb46735b"",
             ""actions"": [
                 {
-                    ""name"": ""Jump"",
-                    ""type"": ""Button"",
-                    ""id"": ""144999de-c8f1-4d06-b182-df36f21575fe"",
-                    ""expectedControlType"": ""Button"",
-                    ""processors"": """",
-                    ""interactions"": """"
-                },
-                {
                     ""name"": ""Time Travel Forward"",
-                    ""type"": ""Value"",
+                    ""type"": ""Button"",
                     ""id"": ""2506a877-31d8-4dca-8adb-b415ceca5afb"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
@@ -44,7 +36,7 @@ public class @InputMaster : IInputActionCollection, IDisposable
                 },
                 {
                     ""name"": ""Time Travel Backward"",
-                    ""type"": ""Value"",
+                    ""type"": ""Button"",
                     ""id"": ""f9e4dfe1-6048-4a73-8a21-7b1e6627b08d"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
@@ -52,7 +44,7 @@ public class @InputMaster : IInputActionCollection, IDisposable
                 },
                 {
                     ""name"": ""Pause"",
-                    ""type"": ""Value"",
+                    ""type"": ""Button"",
                     ""id"": ""33987f7b-f18b-4534-adf9-bfc90692aa73"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
@@ -65,20 +57,65 @@ public class @InputMaster : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Toggle Historic Photos"",
+                    ""type"": ""Button"",
+                    ""id"": ""25021652-9b97-4476-9926-ef1b3b58c8e6"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Run"",
+                    ""type"": ""Button"",
+                    ""id"": ""b8e7e7d3-8415-4780-b9f9-a4968b73741c"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Toggle People"",
+                    ""type"": ""Button"",
+                    ""id"": ""2d37f9b8-7948-453a-a8ef-169f49b2259e"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Start Guided Tour"",
+                    ""type"": ""Button"",
+                    ""id"": ""e065c0b7-79e9-4656-8d2b-572e8248440e"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""End Guided Tour"",
+                    ""type"": ""Button"",
+                    ""id"": ""f3465d85-53ca-4859-bc81-b6a6e3f65b23"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Next Photo"",
+                    ""type"": ""Button"",
+                    ""id"": ""081fbecd-6bfa-4775-954a-b72ac805bff1"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Previous Photo"",
+                    ""type"": ""Button"",
+                    ""id"": ""c10b0693-ef50-42a9-937a-8576f4363d71"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
-                {
-                    ""name"": """",
-                    ""id"": ""04770b1a-cd21-4fc0-979f-566e078d5304"",
-                    ""path"": ""<XInputController>/buttonSouth"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Xbox Control Scheme"",
-                    ""action"": ""Jump"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
                 {
                     ""name"": """",
                     ""id"": ""61fc5951-f868-4cc9-8028-4b4ae96ec5ce"",
@@ -133,6 +170,83 @@ public class @InputMaster : IInputActionCollection, IDisposable
                     ""action"": ""Invert Y Axis"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""51b92423-d964-4fa7-ad21-e8574170b73c"",
+                    ""path"": ""<XInputController>/buttonWest"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Xbox Control Scheme"",
+                    ""action"": ""Toggle Historic Photos"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""ee0d0e04-16b9-480f-94d2-4c8941774934"",
+                    ""path"": ""<XInputController>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Xbox Control Scheme"",
+                    ""action"": ""Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""ec7f7450-c887-4f5d-95e2-914024e16215"",
+                    ""path"": ""<XInputController>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Xbox Control Scheme"",
+                    ""action"": ""Toggle People"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""1a5cebc1-34a4-4ae0-910a-e32efc190e07"",
+                    ""path"": ""<XInputController>/dpad/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Xbox Control Scheme"",
+                    ""action"": ""Start Guided Tour"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5e6a9b1b-85df-406b-aca9-ad1074df40a0"",
+                    ""path"": ""<XInputController>/dpad/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Xbox Control Scheme"",
+                    ""action"": ""End Guided Tour"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""1a7cb498-0bae-4f49-a043-4236c6f3bc6e"",
+                    ""path"": ""<XInputController>/dpad/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Xbox Control Scheme"",
+                    ""action"": ""Next Photo"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""2f0b932f-9c5b-4c87-a76e-411fdc3de876"",
+                    ""path"": ""<XInputController>/dpad/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Xbox Control Scheme"",
+                    ""action"": ""Previous Photo"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -153,12 +267,18 @@ public class @InputMaster : IInputActionCollection, IDisposable
 }");
         // Player
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
-        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
         m_Player_TimeTravelForward = m_Player.FindAction("Time Travel Forward", throwIfNotFound: true);
         m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
         m_Player_TimeTravelBackward = m_Player.FindAction("Time Travel Backward", throwIfNotFound: true);
         m_Player_Pause = m_Player.FindAction("Pause", throwIfNotFound: true);
         m_Player_InvertYAxis = m_Player.FindAction("Invert Y Axis", throwIfNotFound: true);
+        m_Player_ToggleHistoricPhotos = m_Player.FindAction("Toggle Historic Photos", throwIfNotFound: true);
+        m_Player_Run = m_Player.FindAction("Run", throwIfNotFound: true);
+        m_Player_TogglePeople = m_Player.FindAction("Toggle People", throwIfNotFound: true);
+        m_Player_StartGuidedTour = m_Player.FindAction("Start Guided Tour", throwIfNotFound: true);
+        m_Player_EndGuidedTour = m_Player.FindAction("End Guided Tour", throwIfNotFound: true);
+        m_Player_NextPhoto = m_Player.FindAction("Next Photo", throwIfNotFound: true);
+        m_Player_PreviousPhoto = m_Player.FindAction("Previous Photo", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -208,22 +328,34 @@ public class @InputMaster : IInputActionCollection, IDisposable
     // Player
     private readonly InputActionMap m_Player;
     private IPlayerActions m_PlayerActionsCallbackInterface;
-    private readonly InputAction m_Player_Jump;
     private readonly InputAction m_Player_TimeTravelForward;
     private readonly InputAction m_Player_Look;
     private readonly InputAction m_Player_TimeTravelBackward;
     private readonly InputAction m_Player_Pause;
     private readonly InputAction m_Player_InvertYAxis;
+    private readonly InputAction m_Player_ToggleHistoricPhotos;
+    private readonly InputAction m_Player_Run;
+    private readonly InputAction m_Player_TogglePeople;
+    private readonly InputAction m_Player_StartGuidedTour;
+    private readonly InputAction m_Player_EndGuidedTour;
+    private readonly InputAction m_Player_NextPhoto;
+    private readonly InputAction m_Player_PreviousPhoto;
     public struct PlayerActions
     {
         private @InputMaster m_Wrapper;
         public PlayerActions(@InputMaster wrapper) { m_Wrapper = wrapper; }
-        public InputAction @Jump => m_Wrapper.m_Player_Jump;
         public InputAction @TimeTravelForward => m_Wrapper.m_Player_TimeTravelForward;
         public InputAction @Look => m_Wrapper.m_Player_Look;
         public InputAction @TimeTravelBackward => m_Wrapper.m_Player_TimeTravelBackward;
         public InputAction @Pause => m_Wrapper.m_Player_Pause;
         public InputAction @InvertYAxis => m_Wrapper.m_Player_InvertYAxis;
+        public InputAction @ToggleHistoricPhotos => m_Wrapper.m_Player_ToggleHistoricPhotos;
+        public InputAction @Run => m_Wrapper.m_Player_Run;
+        public InputAction @TogglePeople => m_Wrapper.m_Player_TogglePeople;
+        public InputAction @StartGuidedTour => m_Wrapper.m_Player_StartGuidedTour;
+        public InputAction @EndGuidedTour => m_Wrapper.m_Player_EndGuidedTour;
+        public InputAction @NextPhoto => m_Wrapper.m_Player_NextPhoto;
+        public InputAction @PreviousPhoto => m_Wrapper.m_Player_PreviousPhoto;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -233,9 +365,6 @@ public class @InputMaster : IInputActionCollection, IDisposable
         {
             if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
             {
-                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
-                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
-                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
                 @TimeTravelForward.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnTimeTravelForward;
                 @TimeTravelForward.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnTimeTravelForward;
                 @TimeTravelForward.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnTimeTravelForward;
@@ -251,13 +380,31 @@ public class @InputMaster : IInputActionCollection, IDisposable
                 @InvertYAxis.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnInvertYAxis;
                 @InvertYAxis.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnInvertYAxis;
                 @InvertYAxis.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnInvertYAxis;
+                @ToggleHistoricPhotos.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnToggleHistoricPhotos;
+                @ToggleHistoricPhotos.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnToggleHistoricPhotos;
+                @ToggleHistoricPhotos.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnToggleHistoricPhotos;
+                @Run.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRun;
+                @Run.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRun;
+                @Run.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRun;
+                @TogglePeople.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnTogglePeople;
+                @TogglePeople.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnTogglePeople;
+                @TogglePeople.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnTogglePeople;
+                @StartGuidedTour.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnStartGuidedTour;
+                @StartGuidedTour.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnStartGuidedTour;
+                @StartGuidedTour.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnStartGuidedTour;
+                @EndGuidedTour.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnEndGuidedTour;
+                @EndGuidedTour.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnEndGuidedTour;
+                @EndGuidedTour.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnEndGuidedTour;
+                @NextPhoto.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnNextPhoto;
+                @NextPhoto.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnNextPhoto;
+                @NextPhoto.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnNextPhoto;
+                @PreviousPhoto.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPreviousPhoto;
+                @PreviousPhoto.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPreviousPhoto;
+                @PreviousPhoto.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPreviousPhoto;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
             {
-                @Jump.started += instance.OnJump;
-                @Jump.performed += instance.OnJump;
-                @Jump.canceled += instance.OnJump;
                 @TimeTravelForward.started += instance.OnTimeTravelForward;
                 @TimeTravelForward.performed += instance.OnTimeTravelForward;
                 @TimeTravelForward.canceled += instance.OnTimeTravelForward;
@@ -273,6 +420,27 @@ public class @InputMaster : IInputActionCollection, IDisposable
                 @InvertYAxis.started += instance.OnInvertYAxis;
                 @InvertYAxis.performed += instance.OnInvertYAxis;
                 @InvertYAxis.canceled += instance.OnInvertYAxis;
+                @ToggleHistoricPhotos.started += instance.OnToggleHistoricPhotos;
+                @ToggleHistoricPhotos.performed += instance.OnToggleHistoricPhotos;
+                @ToggleHistoricPhotos.canceled += instance.OnToggleHistoricPhotos;
+                @Run.started += instance.OnRun;
+                @Run.performed += instance.OnRun;
+                @Run.canceled += instance.OnRun;
+                @TogglePeople.started += instance.OnTogglePeople;
+                @TogglePeople.performed += instance.OnTogglePeople;
+                @TogglePeople.canceled += instance.OnTogglePeople;
+                @StartGuidedTour.started += instance.OnStartGuidedTour;
+                @StartGuidedTour.performed += instance.OnStartGuidedTour;
+                @StartGuidedTour.canceled += instance.OnStartGuidedTour;
+                @EndGuidedTour.started += instance.OnEndGuidedTour;
+                @EndGuidedTour.performed += instance.OnEndGuidedTour;
+                @EndGuidedTour.canceled += instance.OnEndGuidedTour;
+                @NextPhoto.started += instance.OnNextPhoto;
+                @NextPhoto.performed += instance.OnNextPhoto;
+                @NextPhoto.canceled += instance.OnNextPhoto;
+                @PreviousPhoto.started += instance.OnPreviousPhoto;
+                @PreviousPhoto.performed += instance.OnPreviousPhoto;
+                @PreviousPhoto.canceled += instance.OnPreviousPhoto;
             }
         }
     }
@@ -288,11 +456,17 @@ public class @InputMaster : IInputActionCollection, IDisposable
     }
     public interface IPlayerActions
     {
-        void OnJump(InputAction.CallbackContext context);
         void OnTimeTravelForward(InputAction.CallbackContext context);
         void OnLook(InputAction.CallbackContext context);
         void OnTimeTravelBackward(InputAction.CallbackContext context);
         void OnPause(InputAction.CallbackContext context);
         void OnInvertYAxis(InputAction.CallbackContext context);
+        void OnToggleHistoricPhotos(InputAction.CallbackContext context);
+        void OnRun(InputAction.CallbackContext context);
+        void OnTogglePeople(InputAction.CallbackContext context);
+        void OnStartGuidedTour(InputAction.CallbackContext context);
+        void OnEndGuidedTour(InputAction.CallbackContext context);
+        void OnNextPhoto(InputAction.CallbackContext context);
+        void OnPreviousPhoto(InputAction.CallbackContext context);
     }
 }
