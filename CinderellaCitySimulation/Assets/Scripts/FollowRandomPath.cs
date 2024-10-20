6d80f7081938180a8dda2c8fde3679733a92a68f
Josh <jdeangoldstein@gmail.com>
2024-10-19 20:16:25 -0600

    Cleanup and renaming

--- CinderellaCitySimulation/Assets/Scripts/FollowRandomPath.cs
+++ CinderellaCitySimulation/Assets/Scripts/FollowRandomPath.cs
@@ -19,10 +19,9 @@ public class FollowRandomPath : MonoBehaviour
     public NavMeshPath path;
 
     // variables related to the test for whether the NPC needs to repath
-    readonly bool doRepathIfApproachingPlayer = false;
     readonly bool doRepathIfBlockingPlayerCamera = true;
-    int numberOfFramesApproachingPlayer = 0;
-    readonly int maxNumberOfFramesApproachingPlayer = 60;
+    int numberOfFramesBlockingPlayerCamera = 0;
+    readonly int maxNumberOfFramesBlockingPlayerCamera = 60;
     readonly float dotProductAlignThreshold = 0.90f;
     readonly float dotProductPosThreshold = 0.75f;
     readonly float maxDistanceForFarCheck = 2.0f;
@@ -89,11 +88,11 @@ public class FollowRandomPath : MonoBehaviour
             float currentVelocity = thisAgent.velocity.magnitude;
             Vector3 nextNPCDestination = Utils.GeometryUtils.GetRandomPointOnNavMeshFromPool(this.transform.position, NPCControllerGlobals.initialNPCPositionsArray, NPCControllerGlobals.minDiscardDistance, NPCControllerGlobals.maxDiscardDistance, true);
 
-            // if this NPC is walking the same direction as the player
-            // and blocking the player camera, make them repath
+            // if requested, check if this NPC is close to, aligned, and in front of the camera
+            // which means it may be blocking the camera - so repath
             if (doRepathIfBlockingPlayerCamera)
             {
-                if (GetIsNPCFacingPlayer(this.gameObject, ManageFPSControllers.FPSControllerGlobals.activeFPSController, true))
+                if (GetIsNPCBlockingPlayerCamera(this.gameObject, ManageFPSControllers.FPSControllerGlobals.activeFPSController))
                 {
                     path = NavMeshUtils.SetAgentOnPath(thisAgent, thisAgent.transform.position, nextNPCDestination);
                 }
@@ -101,12 +100,12 @@ public class FollowRandomPath : MonoBehaviour
 
             // if this agent's speed gets too low, it's likely colliding badly with others
             // to prevent a traffic jam, find a different random point from the pool to switch directions
-            if (doRepathIfSlow && currentVelocity > 0f && currentVelocity < NPCControllerGlobals.minimumSpeedBeforeRepath)
+            if (doRepathIfSlow && Mathf.Abs(currentVelocity) < NPCControllerGlobals.minimumSpeedBeforeRepath)
             {
                 path = NavMeshUtils.SetAgentOnPath(thisAgent, thisAgent.transform.position, nextNPCDestination);
             }
 
-            // set next destination
+            // if the agent arrives, set a new destination
             if (thisAgent.remainingDistance <= NPCControllerGlobals.defaultNPCStoppingDistance)
             {
                 path = NavMeshUtils.SetAgentOnPath(thisAgent, thisAgent.transform.position, nextNPCDestination);
@@ -123,7 +122,7 @@ public class FollowRandomPath : MonoBehaviour
         path = this.GetComponent<NavMeshAgent>().path;
     }
 
-    private bool GetIsNPCFacingPlayer(GameObject NPCObject, GameObject FPSController, bool reverseDirection = false /* NPC is facing away from and aligned, in front of the camera */)
+    private bool GetIsNPCBlockingPlayerCamera(GameObject NPCObject, GameObject FPSController)
     {
         // get the NPC direction and compare it to the vector between the NPC and player
         Vector3 NPCForwardVector = (NPCObject.transform.forward).normalized;
@@ -147,54 +146,26 @@ public class FollowRandomPath : MonoBehaviour
             Debug.Log("dotProductAlign: " + dotProductAlignment);
         }
 
-        // reverse direction means the NPC is in front of the camera,
-        // close to, and aligned with the player
-        // so it might block the camera especially during guided tours
-        if (reverseDirection)
+        // if the NPC is in front of the camera, close, and aligned
+        if ((dotProductPosition > dotProductPosThreshold && 
+            dotProductAlignment > dotProductAlignThreshold && 
+            distanceFromNPCToPlayer < maxDistanceForFarCheck))
         {
-            // if the NPC is in front of the camera, close, and aligned
-            if ((dotProductPosition > dotProductPosThreshold && dotProductAlignment > dotProductAlignThreshold && distanceFromNPCToPlayer < maxDistanceForFarCheck))
+            numberOfFramesBlockingPlayerCamera++;
+            if (numberOfFramesBlockingPlayerCamera >= maxNumberOfFramesBlockingPlayerCamera)
             {
-                numberOfFramesApproachingPlayer++;
-                if (numberOfFramesApproachingPlayer >= maxNumberOfFramesApproachingPlayer)
-                {
-                    numberOfFramesApproachingPlayer = 0;
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                numberOfFramesBlockingPlayerCamera = 0;
+                return true;
             }
             else
             {
-                numberOfFramesApproachingPlayer = 0;
                 return false;
             }
         }
         else
         {
-            // if the NPC is looking at and heading towards the player
-            if ((dotProductPosition < dotProductAlignThreshold && dotProductAlignment < dotProductAlignThreshold && distanceFromNPCToPlayer < maxDistanceForFarCheck && thisAgent.velocity.magnitude > 0) || (dotProductAlignment < dotProductPosThreshold && distanceFromNPCToPlayer < maxDistanceForCloseCheck))
-            {
-                numberOfFramesApproachingPlayer++;
-                if (numberOfFramesApproachingPlayer >= maxNumberOfFramesApproachingPlayer)
-                {
-                    numberOfFramesApproachingPlayer = 0;
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                numberOfFramesApproachingPlayer = 0;
-                return false;
-            }
+            numberOfFramesBlockingPlayerCamera = 0;
+            return false;
         }
     }
-
-
 }
