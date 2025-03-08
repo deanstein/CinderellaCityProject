baca6bccd83b3a5e75508197986abddb138cb4e0
Josh <jdeangoldstein@gmail.com>
2025-03-07 22:52:35 -0700

    Splitting recordingMode into autoTimeTravel and shuffleDestinations

--- CinderellaCitySimulation/Assets/Scripts/State/ModeState.cs
+++ CinderellaCitySimulation/Assets/Scripts/State/ModeState.cs
@@ -19,6 +19,9 @@ public static class ModeState
     // if true, guided tour will be enabled automatically when the game loads
     /** this may be overwritten by startup config **/
     public static bool autoStart = false;
+    // if true, guided tour will time-travel automatically after some period
+    /** this may be overwritten by startup config **/
+    public static bool autoTimeTravel = true;
 
     // guided tour causes player to walk and face certain paths, led by its agent
     /** this may be overwritten by startup config **/
@@ -26,6 +29,10 @@ public static class ModeState
     // marked true if a player has taken control of FPSController during guided tour
     // suspension will end automatically after some idle time
     public static bool isGuidedTourPaused = false;
+
+    // shuffle guided tour destinations
+    /** this may be overwritten by startup config **/
+    public static bool shuffleDestinations = false;
 
     /** keep track of guided tour coroutines **/
     /** to ensure only one is active, and allow canceling them if necessary **/
@@ -39,10 +46,6 @@ public static class ModeState
     // FollowGuidedTour.cs looks for these and adjusts object visibility accordingly
     public static bool areHistoricPhotosRequestedVisible = false;
     public static bool arePeopleRequestedVisible = true;
-
-    // recording mode uses a specific scene order for recording videos
-    /** this may be overwritten by startup config **/
-    public static bool recordingMode = false;
 
     //
     // INPUT
